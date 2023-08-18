10f1577abbe34c359955a967b5454942e480b01e
Steph <stephanwilson10@yahoo.com>
2023-08-18 09:01:37 -0400

    uploading still need to fininsh it

--- db/videoGames.js
+++ db/videoGames.js
@@ -1,50 +1,55 @@
-const client = require('./client');
-const util = require('util');
+const client = require("./client");
+const util = require("util");
 
-const REPLACE_ME = 'HELP REPLACE ME!!!!';
+const REPLACE_ME = "HELP REPLACE ME!!!!";
 
 // GET - /api/video-games - get all video games
 async function getAllVideoGames() {
-    try {
-        const { rows: videoGames } = await client.query(REPLACE_ME);
-        return videoGames;
-    } catch (error) {
-        throw new Error("Make sure you have replaced the REPLACE_ME placeholder.")
-    }
+  try {
+    const { rows: videoGames } = await client.query(req.query.videoGame);
+    return videoGames;
+  } catch (error) {
+    throw new Error("Make sure you have replaced the REPLACE_ME placeholder.");
+  }
 }
 
 // GET - /api/video-games/:id - get a single video game by id
 async function getVideoGameById(id) {
-    try {
-        const { rows: [videoGame] } = await client.query(`
+  try {
+    const {
+      rows: [videoGame],
+    } = await client.query(req.params.id
+      `
             SELECT * FROM videoGames
             WHERE id = $1;
-        `, [id]);
-        return videoGame;
-    } catch (error) {
-        throw error;
-    }
+        `,
+      [id]
+    );
+    return videoGame;
+  } catch (error) {
+    throw error;
+  }
 }
 
 // POST - /api/video-games - create a new video game
 async function createVideoGame(body) {
-    // LOGIC GOES HERE
+  // LOGIC GOES HERE
 }
 
 // PUT - /api/video-games/:id - update a single video game by id
 async function updateVideoGame(id, fields = {}) {
-    // LOGIC GOES HERE
+  // LOGIC GOES HERE
 }
 
 // DELETE - /api/video-games/:id - delete a single video game by id
 async function deleteVideoGame(id) {
-    // LOGIC GOES HERE
+  // LOGIC GOES HERE
 }
 
 module.exports = {
-    getAllVideoGames,
-    getVideoGameById,
-    createVideoGame,
-    updateVideoGame,
-    deleteVideoGame
-}
+  getAllVideoGames,
+  getVideoGameById,
+  createVideoGame,
+  updateVideoGame,
+  deleteVideoGame,
+};
